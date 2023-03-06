f0e8169a08c9b67da005197b3ae03bf8a9f8dc79
mewmew88 <mehr_hejazi@hotmail.com>
2023-03-06 20:55:13 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-
-// Variable overrides first
-$primary: #900;
-$enable-shadows: true;
-$prefix: "mo-";
-
-// Then import Bootstrap
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './Components/Pages/Contact';
+import Home from './Components/Pages/Home';
+import Projects from './Components/Pages/Projects';
+import Header from './Components/Header';
 
 function App() {
-  return (
-    <p> This is my portfolio page</p>
-     );
+    return (
+        <Router>
+            <Header />
+            <div>
+                {/* Wrap Route elements in a Routes component */}
+                <Routes>
+                    {/* Define routes using the Route component to render different page components at different paths */}
+                    {/* Define a default route that will render the Home component */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/projects" element={<Projects />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
