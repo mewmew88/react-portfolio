76d43b4d53339d17a9caa60ad61be5fcf8b0ba78
mewmew88 <mehr_hejazi@hotmail.com>
2023-03-06 14:40:50 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+// Variable overrides first
+$primary: #900;
+$enable-shadows: true;
+$prefix: "mo-";
+
+// Then import Bootstrap
+
+
+function App() {
+  return (
+    <p> This is my portfolio page</p>
+     );
+}
+
+export default App;
